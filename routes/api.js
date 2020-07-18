bfa557b32ddb18055431847102553fdcca88a7a0
Hayoun Lee <sidhlee@gmail.com>
2020-07-18 18:52:05 +0900

    Add helmet CSP directives

--- routes/api.js
+++ routes/api.js
@@ -14,5 +14,11 @@ var MongoClient = require('mongodb');
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
 module.exports = function (app) {
-  app.route('/api/stock-prices').get(function (req, res) {});
+  app.route('/api/stock-prices').get(function (req, res) {
+    console.log('GET /api/stock-prices');
+    var stock = req.query.stock;
+    var like = req.query.like || false; // default to false if not included
+    var reqIP = req.connection.remoteAddress;
+    console.log(stock, like, reqIP);
+  });
 };
